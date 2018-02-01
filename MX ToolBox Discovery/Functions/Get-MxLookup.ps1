1e9766d2b947595864a459b480589cfa779822f6
ScottD3v <scottd3v@gmail.com>
2018-02-01 15:02:48 -0700

    Added dynamic paramters

--- MX ToolBox Discovery/Functions/Get-MxLookup.ps1
+++ MX ToolBox Discovery/Functions/Get-MxLookup.ps1
@@ -1,26 +1,63 @@
-# https://mxtoolbox.com/User/Api/Lookup.aspx Function To replace this
-function Get-MxLookup {
+Function Get-MxLookup {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory,
+        [Parameter(Mandatory,Position=0,
         ValueFromPipelineByPropertyName=$True)]
-        [ValidateSet("mx","a","dns","spf","txt","soa","ptr","blacklist","smtp","tcp","http","https","ping","trace")]
+        [ValidateSet("mx","a","dns","spf","txt","soa","ptr","blacklist","smtp","http","https","ping","trace")]
         [string]
-        $Command,
-
-        [Parameter(Mandatory,
-        ValueFromPipelineByPropertyName=$True)]
-        [string]
-        [ValidateScript({
-            If ($_ -like "*.*") {
-              $True
-            }
-            else {
-              Throw "Please enter a domain such as mycompany.com or mycompany.net $_ is not a domain"
-            }
-          })]
-        $Domain
+        $Command
     )
+
+    DynamicParam
+    {
+
+        If ($Command -eq 'ptr')
+        {
+            $attr = New-Object System.Management.Automation.ParameterAttribute
+            $attr.HelpMessage = "An IP address"
+            $attr.Mandatory = $true
+            $attr.ValueFromPipelineByPropertyName = $true
+            $attrColl = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            $attrColl.Add($attr)
+            $attrColl.Add((New-Object System.Management.Automation.ValidateScriptAttribute((({
+                If ($_ -match [IPAddress]$_) {
+                  $True
+                }
+                else {
+                  Throw "Please enter an IP address $_ is not an IP address"
+                }
+              })))))
+            $param = New-Object System.Management.Automation.RuntimeDefinedParameter('IPAddress',[string],$attrColl)
+            $dict = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+            $dict.Add('IPAddress',$param)
+            return $dict
+        }
+
+
+        else {
+
+            $attr = New-Object System.Management.Automation.ParameterAttribute
+                $attr.HelpMessage = "Enter a domain"
+                $attr.Mandatory = $true
+                $attr.ValueFromPipelineByPropertyName = $true
+                $attrColl = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+                $attrColl.Add($attr)
+                $attrColl.Add((New-Object System.Management.Automation.ValidateScriptAttribute((({
+                    If ($_ -match "(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)") {
+                    $True
+                    }
+                    else {
+                    Throw "Please enter a domain such as mycompany.com or mycompany.net $_ is not a domain"
+                    }
+                })))))
+                $param = New-Object System.Management.Automation.RuntimeDefinedParameter('Domain',[string],$attrColl)
+                $dict = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+                $dict.Add('Domain',$param)
+                return $dict
+            
+        } 
+
+    } 
     
     begin {
 
@@ -33,7 +70,7 @@ function Get-MxLookup {
     
     process {
 
-        $URL = "https://api.mxtoolbox.com/api/v1/lookup/$Command/$Domain"
+        $URL = "https://api.mxtoolbox.com/api/v1/lookup/$Command/$($param.Value)"
 
         $results = Invoke-RestMethod -Method GET -Uri $URL -Header $hdrs
     }
@@ -43,5 +80,3 @@ function Get-MxLookup {
         return $results
     }
 }
-
-
