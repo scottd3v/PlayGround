78f46247643ce460d2855916170857b14d58afb8
ScottD3v <scottd3v@gmail.com>
2018-03-15 08:59:32 -0600

    Updated GetGGroupmember

--- Google Admin SDK Discovery/Functions/Get-GGroupmember.ps1
+++ Google Admin SDK Discovery/Functions/Get-GGroupmember.ps1
@@ -1,26 +1,45 @@
-Function Get-GGroupMember {
-    [CmdletBinding()]
-    param (
-
-    [Parameter(Mandatory,ValueFromPipelineByPropertyName,
-    Position=0)]
-    [String]$GroupEmailAddress
+Function GetGGroupMember {	
+    [CmdletBinding()]	
+    param (	
+    
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName,	
+    Position=0)]	
+    [String]$GroupEmailAddress	
+            
+    )	
         
-    )
+    begin {	
     
-    begin {
-    }
+        $resultsArray = @()	
     
-    process {
-
-        $GroupsUri = "https://www.googleapis.com/admin/directory/v1/groups/$GroupEmailAddress/members"
-
-
-        $Results = Invoke-RestMethod -Headers @{Authorization = "Bearer $($GSuiteAdminDirectoryToken.access_token)"} -Uri $GroupsUri  -Method GET -ContentType 'application/json'
-    }
+    }	
+        
+    process {	
     
-    end {
-
-        Return $Results.members.email
-    }
-}
+        $GroupsUri = "https://www.googleapis.com/admin/directory/v1/groups/$GroupEmailAddress/members"	
+    
+    
+        $Results = InvokeRestMethod Headers @{Authorization = "Bearer $($GSuiteAdminDirectoryToken.access_token)"} Uri $GroupsUri  Method GET ContentType 'application/json'	
+    
+        foreach ($user in $Results.members){	
+    
+            $FormattedResult = [pscustomobject] @{	
+    
+                'Group' = $GroupEmailAddress	
+                'User' = $user.email	
+                'Role' = $user.role	
+                'Status' = $user.Status	
+    
+    
+            }	
+    
+            $resultsArray += $FormattedResult	
+    
+        }	
+    }	
+        
+    end {	
+    
+        return $resultsArray	
+    }	
+} 
